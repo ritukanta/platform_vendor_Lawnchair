c31bad093206d583420defd67dd7fde8c05df62e
ritukanta <reddyritukanta1925@gmail.com>
2022-05-30 11:27:06 +0000

    lawnchair: Set as system default prop
    Signed-off-by: ritukanta <reddyritukanta1925@gmail.com>

--- lawnchair.mk
+++ lawnchair.mk
@@ -1,10 +1,7 @@
-include $(call first-makefiles-under,$(LOCAL_PATH))
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.boot.vendor.overlay.static=false
 
 PRODUCT_SOONG_NAMESPACES += \
     vendor/Lawnchair
-
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.boot.vendor.overlay.static=false
 
 PRODUCT_COPY_FILES += \
     vendor/Lawnchair/etc/permissions/privapp-permissions-app.lawnchair.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-app.lawnchair.xml \
